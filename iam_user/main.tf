894c33691d9cb62de9e95550313be476e3aea542
Kiel DeMarco <73503331+KielDemarco@users.noreply.github.com>
2021-03-27 20:33:30 -0700

    Update main.tf

--- iam_user/main.tf
+++ iam_user/main.tf
@@ -7,34 +7,34 @@ terraform {
   }
 }
 #iam group that created user is associated with iam user.
-resource "aws_iam_group" "ec2-mods" {
-  name = "ec2-mods"
+resource "aws_iam_group" "example" {
+  name = "example"
   path = "/users/"
 }
 #creates an iam user. repeat code to make another user.
-resource "aws_iam_user" "ec2-alpha" {
-  name = "ec2-alpha"
+resource "aws_iam_user" "example" {
+  name = "example"
   path = "/system/"
 
   tags = {
     tag-key = "tag-value"
   }
 }
-resource "aws_iam_access_key" "ec2-alpha" {
-  user = aws_iam_user.ec2-alpha.name
+resource "aws_iam_access_key" "example" {
+  user = aws_iam_user.example.name
 }
 data "aws_iam_policy" "AdministratorAccess" {
   arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 #creates user password. repeat code for another user.
-resource "aws_iam_user_login_profile" "ec2-alpha" {
-  user            = aws_iam_user.ec2-alpha.name
+resource "aws_iam_user_login_profile" "example" {
+  user            = aws_iam_user.example.name
   password_length = 10
-  pgp_key         = "keybase:kieldemarco"
+  pgp_key         = "keybase@example"
 }
-resource "local_file" "ec2-alpha_password" {
-  sensitive_content = "-----BEGIN PGP MESSAGE-----\nComment: https://keybase.io/download\nVersion: Keybase Go 1.0.10 (linux)\n\n${aws_iam_user_login_profile.ec2-alpha.encrypted_password}\n-----END PGP MESSAGE-----\n"
-  filename          = "encrypted_password.ec2-alpha.txt"
+resource "local_file" "example" {
+  sensitive_content = "-----BEGIN PGP MESSAGE-----\nComment: https://keybase.io/download\nVersion: Keybase Go 1.0.10 (linux)\n\n${aws_iam_user_login_profile.example.encrypted_password}\n-----END PGP MESSAGE-----\n"
+  filename          = "encrypted_password.example.txt"
   file_permission   = "0600"
 }
 
